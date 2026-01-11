05243a5521111ff1b718f9b18291202b05058c1e
Gregory Garcia <gregory.garcia@authoritypartners.com>
2026-01-11 10:34:25 -0400

    Refresh on navigation.

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -4,8 +4,10 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
+      refetchOnMount: true,
       refetchOnWindowFocus: false,
-      staleTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 0,
+      gcTime: 5 * 60 * 1000 // 5 minutes
     },
     mutations: {
       retry: false,
